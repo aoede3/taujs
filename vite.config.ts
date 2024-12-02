d8431248ded328a0e0c6b0879d8f589d94e4f7f9
aoede <aoedeltd@gmail.com>
2024-12-02 20:28:23 +0000

    feat: Utilise vite-plugin-node-polyfills;

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,14 @@ import path from 'node:path';
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+export const isDevelopment = process.env.NODE_ENV === 'development';
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = join(dirname(__filename), './');
 
 const isSSRBuild = process.env.BUILD_MODE === 'ssr';
 
@@ -12,7 +20,7 @@ export default defineConfig({
     manifest: !isSSRBuild,
     outDir: path.resolve(__dirname, 'dist/client'),
     rollupOptions: {
-      external: ['node:fs/promises', 'node:path', 'node:url', 'node:stream', '@taujs/server', 'react', 'react-dom'],
+      external: ['@taujs/server'],
       input: isSSRBuild
         ? {
             server: path.resolve(__dirname, 'src/client/entry-server.tsx'),
@@ -32,7 +40,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), nodePolyfills({ include: ['fs', 'stream'] })],
   publicDir: 'public',
   resolve: {
     alias: {
