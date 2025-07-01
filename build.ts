ffd33c61e8ca1bb4289633c41ac8dbc099735df0
John Smith <admin@aoede.uk.net>
2025-07-01 20:24:07 +0100

    feat: service typing; cleanup (#11)

--- build.ts
+++ build.ts
@@ -1,18 +1,14 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-import { pluginReact } from '@taujs/react/plugin';
 import { taujsBuild } from '@taujs/server/build';
+
+import { configs } from './buildConfig.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-export const config = [
-  { appId: 'root', entryPoint: '', plugins: [pluginReact()] },
-  { appId: 'mfe', entryPoint: '@admin', plugins: [pluginReact()] },
-];
-
 await taujsBuild({
-  config,
+  configs,
   projectRoot: __dirname,
   clientBaseDir: path.resolve(__dirname, 'src/client'),
 });
