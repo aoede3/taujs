5d72d95deef596e64d717d20f2f3850405c28aa9
John Smith <admin@aoede.uk.net>
2025-07-07 22:46:36 +0100

    feat: taujs config (#12)

--- buildConfig.ts
+++
@@ -1,6 +0,0 @@
-import { pluginReact } from '@taujs/react/plugin';
-
-export const configs = [
-  { appId: 'root', entryPoint: '', plugins: [pluginReact()] },
-  { appId: 'mfe', entryPoint: '@admin', plugins: [pluginReact()] },
-];
