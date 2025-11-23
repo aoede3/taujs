6924f22d0275d83434e691d365190d53e23aada4
aoede <aoedeltd@gmail.com>
2025-11-23 12:15:28 +0000

    chore: integrate taujs/server; packages to 'latest'

--- src/client/entry-server.tsx
+++ src/client/entry-server.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createRenderer } from '@taujs/react';
 
+import type { HeadContext } from '@taujs/react';
+
 import AppBootstrap from '@client/AppBootstrap';
 
-const headContent = (meta: Record<string, unknown>) => `
+const headContent = ({ meta }: HeadContext) => `
   <meta name="description" content="${meta.description || 'τjs [taujs] - Default description'}">
   <link rel="icon" type="image/svg+xml" href="${meta.iconPath || '/taujs.svg?client'}" />
   <title>${meta.title || 'τjs [taujs] - Default title'}</title>
